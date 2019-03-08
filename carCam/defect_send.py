7ed04acc19ff77e098895d0f9e6739f76a0e3863
mis <ch.tseng@sunplusit.com>
2019-03-09 01:18:49 +0800

    update

--- carCam/defect_send.py
+++ carCam/defect_send.py
@@ -1,13 +1,22 @@
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
+
+import sys
 import socket                   # Import socket module
 
-s = socket.socket()             # Create a socket object
-host = "sam.bim-group.com"  #Ip address that the TCPServer  is there
-port = 8021                     # Reserve a port for your service every new transfer wants a new port or you must wait.
+try:
+    s = socket.socket()             # Create a socket object
+    host = "sam.bim-group.com"  #Ip address that the TCPServer  is there
+    port = 8021                     # Reserve a port for your service every new transfer wants a new port or you must wait.
 
-s.connect((host, port))
-#s.send("Hello server!")
+    s.connect((host, port))
+    s.send("GPS (24.2323,211.5325708)")
 
-filename='test.jpg' #In the same folder or path is this file running must the file you want to tranfser to be
+except socket.error as msg:
+    print(msg)
+    sys.exit(1)
+
+filename='../../test.jpg' #In the same folder or path is this file running must the file you want to tranfser to be
 f = open(filename,'rb')
 l = f.read(1024)
 
