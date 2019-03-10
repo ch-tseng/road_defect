d62e4e05a01087eba6c0094c1b8af1d40ebcdb60
Tseng Cheng Hsun <ch.tseng@sunplusit.com>
2019-03-10 22:39:56 +0800

    Add files via upload

--- carCam/defect_send.py
+++ carCam/defect_send.py
@@ -11,37 +11,60 @@ port = 8021                     # Reserve a port for your service every new tran
 defect_org_path = "defect\\original\\"
 
 def send_file(gpsData, org_img_path ):
-    print('Send filename.')
+
     filename = str.encode(gpsData)
-    s.send(filename)
+    send_status = False
 	
-    print('Send defect image.')
+    try:
+        print('Send filename.')	
+        s.send(filename)
+    except:
+        print("Send filename.... got error.")
+        return False
+
     f = open(org_img_path,'rb')
-    l = f.read(2048)	
-    while (l):
-       s.send(l)
-       print('Sent ',repr(l))
-       l = f.read(2048)
-    f.close()
+    l = f.read(1024)
+    try:		
+        print('Send defect image.')
+        while (l):
+           s.send(l)
+           print('Sent ',repr(l))
+           l = f.read(1024)
+        f.close()
+        return True
 
+    except:
+        f.close()
+        print("Send image.... got error.")
+        return False
+		
 if __name__ == "__main__":
-	
-    for file in os.listdir(defect_org_path):
-        try:
-            s = socket.socket()             # Create a socket object
-            s.connect((host, port))
+    conn_status = False
+    while True:	
+        for id, file in enumerate(os.listdir(defect_org_path)):
+            print(id, "Uploading image:", file)
+            while conn_status is False:
+                try:
+                    s = socket.socket()             # Create a socket object
+                    s.connect((host, port))
+                    conn_status = True
 
-        except socket.error as msg:
-            print(msg)
-            sys.exit(1)
+                except socket.error as msg:
+                    print(msg)
+                    #sys.exit(1)
+                    conn_status = False
+                    pass
+				
+            filename, file_extension = os.path.splitext(file)
+            file_extension = file_extension.lower()
 		
-        filename, file_extension = os.path.splitext(file)
-        file_extension = file_extension.lower()
-		
-        if(file_extension == ".jpg" or file_extension==".jpeg" or file_extension==".png" or file_extension==".bmp"):
-            send_file(filename, defect_org_path + file )
+            if(file_extension == ".jpg" or file_extension==".jpeg" or file_extension==".png" or file_extension==".bmp"):
+                upload_status = send_file(filename, defect_org_path + file )
 
-        s.close()
-        print('connection closed')
-        os.remove(defect_org_path + file)
-        time.sleep(1)
+            s.close()
+            conn_status = False
+            print('connection closed')
+            if(upload_status is True):
+                os.remove(defect_org_path + file)
+				
+            time.sleep(1)
