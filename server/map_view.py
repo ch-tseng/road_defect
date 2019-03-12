e2988043f2b255927204ba18d688aaa2a32705b6
ch-tseng <ch.tseng@sunplusit.com>
2019-03-12 23:29:34 +0800

    update

--- server/map_view.py
+++ server/map_view.py
@@ -4,10 +4,12 @@ from flask_googlemaps import Map
 import os
 import cv2
 
+
 static_folder = "20190301"
 target_folder = "20190301/"
 
 #-----------------------------------------
+#https://github.com/rochacbruno/Flask-GoogleMaps
 gps_data = target_folder + "defects.log"
 
 f = open(gps_data, 'r')
@@ -18,11 +20,31 @@ for gps_line in f:
     date_time, gps_data, obj_detect, defect_img_path = gps_line.split("|")
     lat,lng = gps_data.split(',')
     defect_img_path = defect_img_path.replace("\n","")
+    #get detceted classes
+    str_classes = ""
+    objs = obj_detect.split(',')
+    for d_obj in objs:
+        if(len(d_obj)>0):
+            d_classes = d_obj.split(':')
+
+    d_class = d_classes[0]
+    if(d_class=='D12'):
+        icon_path = 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+    elif(d_class=='D20'):
+        icon_path = 'http://maps.google.com/mapfiles/ms/icons/yellow-dot.png'
+    elif(d_class=='D40'):
+        icon_path = 'http://maps.google.com/mapfiles/ms/icons/orange-dot.png'
+    elif(d_class=='D50'):
+        icon_path = 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+    elif(d_class=='D51'):
+        icon_path = 'http://maps.google.com/mapfiles/ms/icons/purple-dot.png'
+    else:
+        icon_path = 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
 
     org_img = target_folder+'originals/'+defect_img_path
     preview_img = target_folder+'previews/'+defect_img_path
     #filename = os.path.basename(defect_img_path)
-    gpsList.append({'icon':'http://maps.google.com/mapfiles/ms/icons/green-dot.png',\
+    gpsList.append({'icon':icon_path,\
         'lat':float(lat), 'lng':float(lng), 'infobox':'<a href="'+org_img+'" target="_blank">\
         <img src="'+preview_img+'" height=60 width=90'+' /></a>' })
 
@@ -39,7 +61,8 @@ def mapview():
         identifier="sndmap",
         lat=24.7356,
         lng=120.9027,
-        markers=gpsList
+        markers=gpsList,
+        style="height:900px;width:1200px;margin:0;"
     )
     return render_template('map01.html', sndmap=sndmap)
 
